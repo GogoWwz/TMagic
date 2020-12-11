ddc067032493debb73459703fd24dd3b62ddd106
GogoWwz <wwz_zjut_web@163.com>
2020-12-11 23:39:06 +0800

    feat:增加按钮组件

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,24 @@
 import React from 'react'
 
+// 按钮类型
 export enum ButtonType {
-    primary: 'primary'
+    Primary = 'primary',
+    Success = "success",
+    Warning = 'warning',
+    Danger = "danger",
+    Info = "info"
+}
+
+// 按钮尺寸
+export enum ButtonSize {
+    Large = 'large',
+    Normal = 'normal',
+    Small = 'small'
+}
+
+
+const Button: React.FC = props => {
+    return (
+        <button>按钮组件</button>
+    )
 }
