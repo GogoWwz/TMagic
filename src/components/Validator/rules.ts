a69b7b5d165e0e03bb8c6f80c6279e8bd35428bd
GogoWwz <wwz_zjut_web@163.com>
2021-01-12 22:55:38 +0800

    feat: validator数字校验器的开发

---
+++ src/components/Validator/rules.ts
@@ -0,0 +1,5 @@
+const Num: RegExp = /^[0-9]*$/
+
+export {
+  Num
+}
--- src/components/Validator/validator.tsx
+++ src/components/Validator/validator.tsx
@@ -1,40 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classnames from 'classnames'
 
-const ChildrenLegalName: Array<string> = [
-  "Input"
-]
+import {
+  Num
+} from './rules'
 
-export interface ValidatorProps {
-  classNames?: string,
+// const ChildrenLegalName: Array<string> = [
+//   "Input"
+// ]
+
+interface IValidator {
+  rule?: RegExp,
   message?: string
 }
 
-interface FunctionComponentElement {}
+export interface ValidatorProps {
+  validates: {
+    num?: IValidator,
+    custom?: IValidator
+  },
+  classNames?: string
+}
+
+// interface FunctionComponentElement {}
 
 const Validator: React.FC<ValidatorProps> = props => {
-  const { classNames, children, message } = props
-
-  const ChildrenElement = children as React.FunctionComponentElement<FunctionComponentElement>
-
-  const { displayName = "" } = ChildrenElement.type
-
-  if(!ChildrenLegalName.includes(displayName)) {
-    console.error("Tmagic Warning: Validator component must accept legal verifiable children！")
-  }
+  const { validates, classNames, children } = props
+  const [isValid, setIsValid] = useState(true)
+  const [mes, setMes] = useState("")
 
   const classes = classnames("tm-vali", classNames)
 
+  // 校验器进行校验
+  const validate = (val: string) => {
+    Object.keys(validates).forEach(item => {
+      if(validates.hasOwnProperty(item)) {
+        const { message } = (validates as any)[item]
+        if(item === 'num') {
+          setIsValid(Num.test(val))
+          setMes(message || "请输入纯数字")
+        }
+      }
+    })
+  }
+  // const ChildrenElement = children as React.FunctionComponentElement<FunctionComponentElement>
+  // const { displayName = "" } = ChildrenElement.type
+  // if(!ChildrenLegalName.includes(displayName)) {
+  //   console.error("Tmagic Warning: Validator component must accept legal verifiable children！")
+  // }
+  const changeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    validate(e.target.value)
+  }
   return (
     <div className={classes}>
       { children }
-      <div className="tm-vali__err">{ message }</div>
+      <input type="text" onChange={changeInput} />
+      {
+        !isValid && <div className="tm-vali__err">{ mes }</div>
+      }
     </div>
   )
 }
 
-Validator.defaultProps = {
-  message: "请输入合法的值"
-}
+Validator.defaultProps = {}
 
 export default Validator
