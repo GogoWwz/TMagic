21fded5dddfc4e073c5cf4e515df774a1b8a72f9
GogoWwz <wwz_zjut_web@163.com>
2021-01-14 23:32:47 +0800

    feat: validator组件自定义规则的开发

--- src/components/Validator/validator.tsx
+++ src/components/Validator/validator.tsx
@@ -8,7 +8,7 @@ const ChildrenLegalName: Array<string> = [
 ]
 
 interface IValidator {
-  rule?: RegExp,
+  rule?: ((value: string) => boolean) | RegExp,
   message?: string,
   value?: number
 }
@@ -26,7 +26,7 @@ interface FunctionComponentElement {}
 
 const Validator: React.FC<ValidatorProps> = props => {
   const { validates, classNames, children } = props
-  const [isValid, setIsValid] = useState(true)
+  const [valid, setValid] = useState(true)
   const [mes, setMes] = useState("")
 
   const classes = classnames("tm-vali", classNames)
@@ -38,24 +38,45 @@ const Validator: React.FC<ValidatorProps> = props => {
 
   const validate = (val: string) => {
     let valueType = "string"
+
     if(valideRules.includes("num")) {
       const { message } = validates["num"] as IValidator
-      const flag = _.isNum(val)
-      setIsValid(flag)
+      const numValid = _.isNum(val)
       valueType = "number"
-      if(!flag) {
+      setValid(numValid)
+      if(!numValid) {
         setMes(message || "请输入纯数字")
         return
       }
     }
+
     if(valideRules.includes("min")) {
-      const { message, value } = validates["min"] as IValidator
+      const { message, value = 10 } = validates["min"] as IValidator
+      let minValid = _.isAboveLen(val, value)
       if(valueType === 'number') {
-        setIsValid(_.isAboveNum(val, value || 10))
-      } else {
-        setIsValid(_.isAboveLen(val, value || 10))
+        minValid = _.isAboveNum(val, value)
       }
-      setMes(message || "请输入正确范围的值")
+      setValid(minValid)
+      if(!minValid) {
+        setMes(message || "请输入正确范围的值")
+        return
+      }
+    }
+
+    if(valideRules.includes("custom")) {
+      const { rule, message } = validates["custom"] as IValidator
+      if(typeof rule !== 'function') {
+        console.error("warning: custom rule must be a function！")
+        return
+      }
+      const customValid = rule(val);
+      if(typeof customValid !== 'boolean') {
+        console.error("warning: custom rule must return a boolean！")
+      }
+      setValid(customValid)
+      if(!customValid) {
+        setMes(message || "自定义校验规则失败")
+      }
     }
   }
   // 渲染子组件
@@ -72,7 +93,7 @@ const Validator: React.FC<ValidatorProps> = props => {
       { children }
       <input type="text" onChange={changeInput} />
       {
-        !isValid && <div className="tm-vali__err">{ mes }</div>
+        !valid && <div className="tm-vali__err">{ mes }</div>
       }
     </div>
   )
